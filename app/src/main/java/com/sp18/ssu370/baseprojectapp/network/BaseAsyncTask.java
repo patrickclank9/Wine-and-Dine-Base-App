84d8943508e93ccaf3dcaea5d4a978a5a25076cd
Tyler Holland <hollandt@sonoma.edu>
2018-03-14 18:45:47 -0700

    finish generic typing on BaseAsyncTask

--- app/src/main/java/com/sp18/ssu370/baseprojectapp/network/BaseAsyncTask.java
+++ app/src/main/java/com/sp18/ssu370/baseprojectapp/network/BaseAsyncTask.java
@@ -5,15 +5,15 @@ import android.os.AsyncTask;
 
 
 public abstract class BaseAsyncTask<Params, Progress, Result>
-        extends AsyncTask <Params, Progress, Result>{
+        extends AsyncTask<Params, Progress, Result> {
 
-    protected CallbackListener listener;
+    protected CallbackListener<Result> listener;
 
-    public void setListener(CallbackListener listenerDelegate) {
+    public void setListener(CallbackListener<Result> listenerDelegate) {
         listener = listenerDelegate;
     }
 
-    public interface CallbackListener <Result> {
+    public interface CallbackListener<Result> {
         void onCallback(Result result);
     }
 }
