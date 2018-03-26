73cbd1fabcc210e8446b40def601e47a7f840525
Martin Plut <36430643+ObviouslyMartin@users.noreply.github.com>
2018-03-25 18:43:41 -0700

    maps
    maps integration

--- app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MainActivity.java
+++ app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MainActivity.java
@@ -1,8 +1,11 @@
 package com.sp18.ssu370.baseprojectapp.ui.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.sp18.ssu370.baseprojectapp.MapsActivity;
 import com.sp18.ssu370.baseprojectapp.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    //called when user taps find location
+    public void FindLocation(View view){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
