f5bd9a9a1eec459da64909be4c8ecce4caf4fee7
ObviouslyMartin <36430643+ObviouslyMartin@users.noreply.github.com>
2018-04-23 17:20:42 -0700

    MapsComplete
    Aww yis!

--- app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MapsActivity.java
+++ app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MapsActivity.java
@@ -6,70 +6,91 @@ import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
+import android.text.Html;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.PlaceBufferResponse;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.RuntimeRemoteException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.sp18.ssu370.baseprojectapp.PlaceAutocompleteAdapter;
 import com.sp18.ssu370.baseprojectapp.R;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-//import com.google.android.gms.location.places.Places;
-//import com.google.android.gms.location.places.PlaceDetectionClient;
-//import com.google.android.gms.location.places.GeoDataClient;
 
-public class
-MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    //private boolean myLocation = true;
+    private Place mPlace;
     private boolean mLocationPermissionGranted = false;
     private FusedLocationProviderClient mFusedLocationProviderClient;
+    private PlaceAutocompleteAdapter placeAutocompleteAdapter;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1997;
     private static final float DEFAULT_ZOOM = 15f;
+    private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(
+            new LatLng(-40, -168), new LatLng(71, 136));
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
-    //private GeoDataClient mGeoDataClient;
-    //private PlaceDetectionClient mPlaceDetectionClient;
-
+    private AutoCompleteTextView editText;
+    protected GeoDataClient mGeoDataClient;
+    protected PlaceDetectionClient mPlaceDetectionClient;
 
     @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        mGeoDataClient = Places.getGeoDataClient(this, null);
+        mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+        editText = findViewById(R.id.editText);
         getLocationPermission();
-        init();
+
         //getDeviceLocation();
     }
-    private void init(){
-        EditText locationSearch = findViewById(R.id.editText);
-        locationSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+    private void init() {
+        editText.setOnItemClickListener(mAutocompleteClickLister);
+        placeAutocompleteAdapter = new PlaceAutocompleteAdapter(this, mGeoDataClient,
+                LAT_LNG_BOUNDS, null);
+
+        editText.setAdapter(placeAutocompleteAdapter);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_SEARCH
@@ -77,76 +98,79 @@ MapsActivity extends FragmentActivity implements OnMapReadyCallback {
                         || event.getAction() == KeyEvent.ACTION_DOWN
                         || event.getAction() == KeyEvent.KEYCODE_ENTER) {
 
+                    hideSoftKeyboard();
                     geoLocate();
                 }
                 return false;
             }
         });
     }
-    private void geoLocate(){
-        EditText locationSearch = findViewById(R.id.editText);
-        String location = locationSearch.getText().toString();
-        List<Address> addressList = null;
-        if(location != null || !location.equals("")){
-            Geocoder geocoder = new Geocoder(MapsActivity.this);
-            try{
-                addressList = geocoder.getFromLocationName(location, 1);
-            } catch (IOException e){
-                e.printStackTrace();
-            }
-            Address address = addressList.get(0);
-            String string = addressList.get(0).getLocality() + ", ";
-            string += addressList.get(0).getCountryName();
-            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-            locationSearch.clearComposingText();
-            mMap.clear();
-            mMap.addMarker(new MarkerOptions().position(latLng).title(string));
-            //computeHeading(C);
-            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-            hideSoftKeyboard();
+
+    private void geoLocate() {
+        String searchString = editText.getText().toString();
+        Geocoder geocoder = new Geocoder(MapsActivity.this);
+        List<Address> list = new ArrayList<>();
+        try {
+            list = geocoder.getFromLocationName(searchString, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (list.size() > 0) {
+            Address address = list.get(0);
+
+            moveCamera(new LatLng(address.getLatitude(), address.getLongitude()), DEFAULT_ZOOM, address.getAddressLine(0));
         }
     }
-    private void getDeviceLocation(){
+
+    private void getDeviceLocation() {
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        try{
-            if(mLocationPermissionGranted){
-                Task location = mFusedLocationProviderClient.getLastLocation();
+        try {
+            if (mLocationPermissionGranted) {
+                final Task location = mFusedLocationProviderClient.getLastLocation();
                 location.addOnCompleteListener(new OnCompleteListener() {
                     @Override
                     public void onComplete(@NonNull Task task) {
-                        if(task.isSuccessful()){
-                            Location currentLocation = (Location)task.getResult();
+                        if (task.isSuccessful()) {
+                            Location currentLocation = (Location) task.getResult();
                             moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLatitude()),
-                                    DEFAULT_ZOOM);
-                        }
-                        else {
+                                    DEFAULT_ZOOM, "Your Location");
+                        } else {
                             Toast.makeText(MapsActivity.this, "unable to get current Location", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
             }
-        }catch (SecurityException e){
-            //Log.e(TAG, "get DeviceLoction: Security Exception" + e.getMessage());
+        } catch (SecurityException e) {
+            e.printStackTrace();
         }
     }
-    private void moveCamera(LatLng latLng, float zoom){
+
+    private void moveCamera(LatLng latLng, float zoom, String title) {
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
         hideSoftKeyboard();
+        if (!title.equals("My Location")) {
+            MarkerOptions options = new MarkerOptions()
+                    .position(latLng)
+                    .title(title);
+            mMap.addMarker(options);
+        }
     }
-    private void initMap(){
+
+    private void initMap() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(MapsActivity.this);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] myPermissions, @NonNull int[]grantResults){
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] myPermissions, @NonNull int[] grantResults) {
         mLocationPermissionGranted = false;
-        switch (requestCode){
-            case LOCATION_PERMISSION_REQUEST_CODE:{
-                if(grantResults.length > 0){
-                    for(int i = 0; i < grantResults.length; i++){
-                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
                             mLocationPermissionGranted = false;
                             return;
                         }
@@ -167,6 +191,7 @@ MapsActivity extends FragmentActivity implements OnMapReadyCallback {
                 PackageManager.PERMISSION_GRANTED &&
                 ContextCompat.checkSelfPermission(this.getApplicationContext(), COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mLocationPermissionGranted = true;
+            initMap();
         } else {
             ActivityCompat.requestPermissions(this, MyPermissions, LOCATION_PERMISSION_REQUEST_CODE);
         }
@@ -175,39 +200,57 @@ MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        if (mLocationPermissionGranted){
+        if (mLocationPermissionGranted) {
             getDeviceLocation();
-
-        }
-        //mMap.setMyLocationEnabled(true);
-
-    }
-    public void onMapSearch(View view){
-        EditText locationSearch = findViewById(R.id.editText);
-        String location = locationSearch.getText().toString();
-        List<Address> addressList = null;
-        if(location != null || !location.equals("")){
-            Geocoder geocoder = new Geocoder(MapsActivity.this);
-            try{
-                addressList = geocoder.getFromLocationName(location, 1);
-            } catch (IOException e){
-                e.printStackTrace();
+            if (ActivityCompat.checkSelfPermission((this), Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
             }
-            Address address = addressList.get(0);
-            String string = addressList.get(0).getLocality() + ", ";
-            string += addressList.get(0).getCountryName();
-            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-            //locationSearch.clearComposingText();
-            //mMap.clear();
-            mMap.addMarker(new MarkerOptions().position(latLng).title(string));
-            //computeHeading(C);
-            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-            hideSoftKeyboard();
+            mMap.setMyLocationEnabled(true);
+            init();
         }
     }
 
-    private void hideSoftKeyboard(){
+    public void onMapSearch(View view) {
+        geoLocate();
+    }
+
+    private void hideSoftKeyboard() {
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
+
+    private AdapterView.OnItemClickListener mAutocompleteClickLister = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            hideSoftKeyboard();
+            final AutocompletePrediction item = placeAutocompleteAdapter.getItem(position);
+            final String placeId = item.getPlaceId();
+
+            Task<PlaceBufferResponse> placeResult = mGeoDataClient.getPlaceById(placeId);
+            placeResult.addOnCompleteListener(mUpdatePlaceDetailsCallback);
+        }
+
+        private OnCompleteListener<PlaceBufferResponse> mUpdatePlaceDetailsCallback
+                = new OnCompleteListener<PlaceBufferResponse>() {
+            @Override
+            public void onComplete(Task<PlaceBufferResponse> task) {
+                try {
+                    PlaceBufferResponse places = task.getResult();
+
+                    // Get the Place object from the buffer.
+                    final Place place = places.get(0);
+                    moveCamera(new LatLng(place.getViewport().getCenter().latitude,
+                                    place.getViewport().getCenter().longitude), DEFAULT_ZOOM,
+                            place.getName().toString());
+
+                    places.release();
+                } catch (RuntimeRemoteException e) {
+                    // Request did not complete successfully
+                    return;
+                }
+            }
+        };
+    };
 }
 
