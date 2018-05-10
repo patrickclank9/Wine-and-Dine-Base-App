b1fd5b06c420c3a009a847ac0c0614e9b17e126e
willmclaughlin13 <willmclaughlin13@gmail.com>
2018-05-09 18:46:33 -0700

    YESSSSSSSSS

--- app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/ProfileActivity.java
+++ app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.sp18.ssu370.baseprojectapp.ui.activities;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,14 +13,46 @@ import com.sp18.ssu370.baseprojectapp.R;
 
 public class ProfileActivity extends AppCompatActivity {
     Dialog winePreferenceDialog;
+    Dialog foodPreferenceDialog;
+    Dialog matchesDialog;
+    private Button creatprofilebutton;
+    private Button winepreferencebutton;
+    private Button foodpreferencebutton;
+    private Button matchbutton;
+    private ImageButton profilebutton;
+    private ImageButton mainactivitybutton;
+    private ImageButton mapbutton;
 
-  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         winePreferenceDialog = new Dialog(this);
-    }
 
+        profilebutton = findViewById(R.id.toProfileActivity);                                          // Profile button
+        profilebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this, ProfileActivity.class));
+            }
+        });
+
+        mainactivitybutton = findViewById(R.id.toMainActivity);                                        // Home button
+        mainactivitybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+            }
+        });
+
+        mapbutton = findViewById(R.id.FindLoc);                                                       // Map button
+        mapbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this, MapsActivity.class));
+            }
+
+        });
+    }
 }
 
